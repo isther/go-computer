d6a02f544b06e55aec988acc88ed0c0d5525f7c4
isther <28251536@qq.com>
2022-05-22 19:36:12 +0800

    Simplified test case format

--- circuit/gate/gate_test.go
+++ circuit/gate/gate_test.go
@@ -9,24 +9,17 @@ func TestNOTGate(t *testing.T) {
 	tests := []struct {
 		name   string
 		valueA bool
-		want   bool
+		expect bool
 	}{
-		{
-			name:   "1",
-			valueA: false,
-			want:   true,
-		}, {
-			name:   "2",
-			valueA: true,
-			want:   false,
-		},
+		{"1", false, true},
+		{"2", true, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			notGate := NewNOTGate()
 
-			if value := notGate.Update(tt.valueA).Value(); !reflect.DeepEqual(value, tt.want) {
-				t.Errorf("NOTGate-%s: value: %v result: %v want: %v", tt.name, tt.valueA, value, tt.want)
+			if value := notGate.Update(tt.valueA).Value(); !reflect.DeepEqual(value, tt.expect) {
+				t.Errorf("NOTGate-%s: value: %v result: %v   %v", tt.name, tt.valueA, value, tt.expect)
 			}
 		})
 	}
@@ -37,36 +30,19 @@ func TestORGate(t *testing.T) {
 		name   string
 		valueA bool
 		valueB bool
-		want   bool
+		expect bool
 	}{
-		{
-			name:   "1",
-			valueA: false,
-			valueB: false,
-			want:   false,
-		}, {
-			name:   "2",
-			valueA: true,
-			valueB: false,
-			want:   true,
-		}, {
-			name:   "3",
-			valueA: false,
-			valueB: true,
-			want:   true,
-		}, {
-			name:   "4",
-			valueA: true,
-			valueB: true,
-			want:   true,
-		},
+		{"1", false, false, false},
+		{"2", true, false, true},
+		{"3", false, true, true},
+		{"4", true, true, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			orGate := NewORGate()
 
-			if value := orGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.want) {
-				t.Errorf("ORGate-%s: value: %v %v result: %v want: %v", tt.name, tt.valueA, tt.valueB, value, tt.want)
+			if value := orGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.expect) {
+				t.Errorf("ORGate-%s: value: %v %v result: %v   %v", tt.name, tt.valueA, tt.valueB, value, tt.expect)
 			}
 		})
 	}
@@ -77,36 +53,19 @@ func TestNORGate(t *testing.T) {
 		name   string
 		valueA bool
 		valueB bool
-		want   bool
+		expect bool
 	}{
-		{
-			name:   "1",
-			valueA: false,
-			valueB: false,
-			want:   true,
-		}, {
-			name:   "2",
-			valueA: true,
-			valueB: false,
-			want:   false,
-		}, {
-			name:   "3",
-			valueA: false,
-			valueB: true,
-			want:   false,
-		}, {
-			name:   "4",
-			valueA: true,
-			valueB: true,
-			want:   false,
-		},
+		{"1", false, false, true},
+		{"2", true, false, false},
+		{"3", false, true, false},
+		{"4", true, true, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			norGate := NewNORGate()
 
-			if value := norGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.want) {
-				t.Errorf("NORGate-%s: value: %v %v result: %v want: %v", tt.name, tt.valueA, tt.valueB, value, tt.want)
+			if value := norGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.expect) {
+				t.Errorf("NORGate-%s: value: %v %v result: %v   %v", tt.name, tt.valueA, tt.valueB, value, tt.expect)
 			}
 		})
 	}
@@ -117,36 +76,19 @@ func TestANDGate(t *testing.T) {
 		name   string
 		valueA bool
 		valueB bool
-		want   bool
+		expect bool
 	}{
-		{
-			name:   "1",
-			valueA: false,
-			valueB: false,
-			want:   false,
-		}, {
-			name:   "2",
-			valueA: true,
-			valueB: false,
-			want:   false,
-		}, {
-			name:   "3",
-			valueA: false,
-			valueB: true,
-			want:   false,
-		}, {
-			name:   "4",
-			valueA: true,
-			valueB: true,
-			want:   true,
-		},
+		{"1", false, false, false},
+		{"2", true, false, false},
+		{"3", false, true, false},
+		{"4", true, true, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			andGate := NewANDGate()
 
-			if value := andGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.want) {
-				t.Errorf("ANDGate-%s: value: %v %v result: %v want: %v", tt.name, tt.valueA, tt.valueB, value, tt.want)
+			if value := andGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.expect) {
+				t.Errorf("ANDGate-%s: value: %v %v result: %v   %v", tt.name, tt.valueA, tt.valueB, value, tt.expect)
 			}
 		})
 	}
@@ -157,36 +99,19 @@ func TestNANDGate(t *testing.T) {
 		name   string
 		valueA bool
 		valueB bool
-		want   bool
+		expect bool
 	}{
-		{
-			name:   "1",
-			valueA: false,
-			valueB: false,
-			want:   true,
-		}, {
-			name:   "2",
-			valueA: true,
-			valueB: false,
-			want:   true,
-		}, {
-			name:   "3",
-			valueA: false,
-			valueB: true,
-			want:   true,
-		}, {
-			name:   "4",
-			valueA: true,
-			valueB: true,
-			want:   false,
-		},
+		{"1", false, false, true},
+		{"2", true, false, true},
+		{"3", false, true, true},
+		{"4", true, true, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			nandGate := NewNANDGate()
 
-			if value := nandGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.want) {
-				t.Errorf("NANDGate-%s: value: %v %v result: %v want: %v", tt.name, tt.valueA, tt.valueB, value, tt.want)
+			if value := nandGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.expect) {
+				t.Errorf("NANDGate-%s: value: %v %v result: %v   %v", tt.name, tt.valueA, tt.valueB, value, tt.expect)
 			}
 		})
 	}
@@ -197,36 +122,19 @@ func TestXORGate(t *testing.T) {
 		name   string
 		valueA bool
 		valueB bool
-		want   bool
+		expect bool
 	}{
-		{
-			name:   "1",
-			valueA: false,
-			valueB: false,
-			want:   false,
-		}, {
-			name:   "2",
-			valueA: true,
-			valueB: false,
-			want:   true,
-		}, {
-			name:   "3",
-			valueA: false,
-			valueB: true,
-			want:   true,
-		}, {
-			name:   "4",
-			valueA: true,
-			valueB: true,
-			want:   false,
-		},
+		{"1", false, false, false},
+		{"2", true, false, true},
+		{"3", false, true, true},
+		{"4", true, true, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			xorGate := NewXORGate()
 
-			if value := xorGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.want) {
-				t.Errorf("XORGate-%s: value: %v %v result: %v want: %v", tt.name, tt.valueA, tt.valueB, value, tt.want)
+			if value := xorGate.Update(tt.valueA, tt.valueB).Value(); !reflect.DeepEqual(value, tt.expect) {
+				t.Errorf("XORGate-%s: value: %v %v result: %v   %v", tt.name, tt.valueA, tt.valueB, value, tt.expect)
 			}
 		})
 	}
